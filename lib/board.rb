91e5caaa4522b3b28efa03151497129dac2b4931
Kasuhira <hheflerdevelopment@gmail.com>
2020-01-23 17:58:17 -0300

    Fixed linter issues

--- lib/board.rb
+++ lib/board.rb
@@ -2,7 +2,7 @@ class Board
   attr_accessor :board
   attr_reader :winning_lines
 
-  public def initialize
+  def initialize
     @winning_lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]]
     @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Board do
   describe '#initialize' do
     it 'returns a 3 by 3 grid in correct order' do
       board = Board.new
-      expect(board.initialize).to match_array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+      expect(board.board).to match_array([1, 2, 3, 4, 5, 6, 7, 8, 9])
     end
   end
 
